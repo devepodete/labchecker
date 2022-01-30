95b008b1554eec362eeb7fd7c603341bb69a6dad
devepodete <remax_2000@mail.ru>
2022-01-30 23:36:34 +0300

    implement ExecutableBuilder

--- Pipeline/ExecutableBuilder.py
+++ Pipeline/ExecutableBuilder.py
@@ -1,9 +1,25 @@
-from PipelineBase import IPipelineElement
+from PipelineBase import IPipelineElement, Verdict
+
+import subprocess
+from typing import List
 
 
 class ExecutableBuilder(IPipelineElement):
-    def __init__(self):
+    def __init__(self, compiler: str, compiler_flags: List[str], source_file: str, output_file: str):
         super().__init__('Build')
+        self.compiler = compiler
+        self.compilerFlags = compiler_flags
+        self.sourceFile = source_file
+        self.outputFile = output_file
 
     def execute(self) -> None:
-        pass
+        res = subprocess.run([self.compiler, *self.compilerFlags, self.sourceFile, '-o', self.outputFile],
+                             stdout=subprocess.DEVNULL,
+                             stderr=subprocess.PIPE)
+
+        if res.returncode != 0:
+            if res.stderr:
+                self.executionResult.message = res.stderr.decode('utf-8')
+            self.executionResult.verdict = Verdict.FAIL
+        else:
+            self.executionResult.verdict = Verdict.OK
