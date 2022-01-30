659a827efc376a85ccb219f98a42db0f96ddcff0
devepodete <remax_2000@mail.ru>
2022-01-30 23:36:14 +0300

    improve report generation

--- StudentInteraction/ReportGeneration/ReportGenerator.py
+++ StudentInteraction/ReportGeneration/ReportGenerator.py
@@ -22,35 +22,36 @@ class ReportType:
 
 
 class ReportGenerator:
-    def __init__(self, gates: List[Gate], output: str, report_type: ReportType):
+    def __init__(self, gates: List[Gate]):
         self.gates = gates
-        self.output = output
-        self.reportType = report_type
+        self.report = ''
 
-    def generate_report(self) -> None:
-        if self.reportType == ReportType.Text:
-            report = self.generate_text()
-        elif self.reportType == ReportType.Html:
-            report = self.generate_html()
+    def generate_report(self, report_type: ReportType) -> None:
+        if report_type == ReportType.Text:
+            self.generate_text()
+        elif report_type == ReportType.Html:
+            self.generate_html()
         else:
             raise NotImplementedError
 
-        if self.output is not None and len(self.output) != 0:
-            with open(self.output, 'w') as f:
-                f.write(report)
+    def save_to(self, path: str) -> None:
+        with open(path, 'w') as f:
+            f.write(self.report)
 
-    def generate_text(self) -> str:
+    def generate_text(self) -> None:
         report = ''
         for gate in self.gates:
             report += f'\nGATE {gate.name}\n'
 
             for pipelineElem in gate.pipeline:
                 result = pipelineElem.get_result()
-                report += f'{result.name}: {result.verdict}{result.message}\n'
+                report += f'{result.name}: {result.verdict}{result.verdictInfo}\n'
+                if len(result.message) != 0:
+                    report += f'Message (first 1000 chars): `{result.message[:1000]}\''
 
-        return report
+        self.report = report
 
-    def generate_html(self) -> str:
+    def generate_html(self) -> None:
         report = "<table border=1 style='text-align:center; vertical-align:middle'>"
         report += "<caption>Verdict</caption>"
 
@@ -60,13 +61,19 @@ class ReportGenerator:
         report += "</tr>"
 
         report += "<tr>"
+        message = ''
         for gate in self.gates:
             for pipelineElem in gate.pipeline:
                 res = pipelineElem.get_result()
                 report += f"<td>{res.name}</td>"
-                report += f"<td bgcolor={VERDICT_COLOR[res.verdict]}>{res.verdict}{res.message}</td>"
+                report += f"<td bgcolor={VERDICT_COLOR[res.verdict]}>{res.verdict}{res.verdictInfo}</td>"
+                if len(res.message) != 0 and len(message) < 1000:
+                    message += res.message[:1000]
+
         report += "</tr>"
 
         report += "</table>"
+        if len(message) != 0:
+            report += f'Message (first 1000 chars): {message}'
 
-        return report
+        self.report = report
