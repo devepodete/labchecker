b2fb606a1a2f79119f862298d278e7d2770ee1e8
devepodete <remax_2000@mail.ru>
2022-01-30 23:36:01 +0300

    add verdictInfo to ExecutionResult

--- PipelineBase/ExecutionResult.py
+++ PipelineBase/ExecutionResult.py
@@ -2,7 +2,8 @@ from .Verdict import Verdict
 
 
 class ExecutionResult:
-    def __init__(self, name: str, verdict: Verdict, message=''):
+    def __init__(self, name: str, verdict: Verdict, verdict_info='', message=''):
         self.name = name
         self.verdict = verdict
+        self.verdictInfo = verdict_info
         self.message = message
